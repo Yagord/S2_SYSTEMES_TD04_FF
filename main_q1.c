2a87798614628c2e983e3e4821510d233970e378
Yagord <39037512+Yagord@users.noreply.github.com>
2018-06-25 14:52:29 +0200

    Add files via upload

---
+++ main_q1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char chaine[8] = "bonjour";
+	printf("Chaine : %s\n", chaine);
+	printf("Taille : %d\n", 6);
+	printf("Adresse : %d\n", chaine);
+	return 0;
+}
